7fbf3449de2f905e22053504fecc4ce7444f7be2
JorgeJaraJ <83614934+JorgeJaraJ@users.noreply.github.com>
2022-10-27 00:17:27 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,18 +1,58 @@
 public class Card {
 
-    private final String palo;
-    private final String color;
-    private final String valor;
+    private String valor;
+    private String palo;
+    private String color;
 
-    public Card(String palo, String color, String valor) {
+    Card(String palo, String color, String valor) {
+        this.valor = valor;
         this.palo = palo;
         this.color = color;
-        this.valor = valor;
     }
 
-    @Override
+    Card(String palo, String color) {
+        this.palo = palo;
+        this.color = color;
+    }
+
+    public String getPalo() {
+        return this.palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getValor() {
+        return this.valor;
+    }
+
+    public void setValor(Integer valor) {
+        if (valor <= 10) {
+            if (valor == 1) {
+                this.valor = "A";
+            }
+
+            this.valor = valor.toString();
+        } else if (valor == 11) {
+            this.valor = "J";
+        } else if (valor == 12) {
+            this.valor = "Q";
+        } else {
+            this.valor = "k";
+        }
+
+    }
+
     public String toString() {
-        return "Palo: " + palo + " | " + "Color: " + color +
-                " | " + "Valor: " + valor;
+        return "valor=" + this.valor + ", palo=" + this.palo + ",color=" + this.color;
     }
 }
